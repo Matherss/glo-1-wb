54ed64601445ec3287323103d5f2542a004bf003
Matherss <mathers_beats@mail.ru>
2022-02-23 12:45:36 +0300

    refactor

--- js/search.js
+++ js/search.js
@@ -2,7 +2,7 @@ const search = function () {
   const input = document.querySelector(".search-block > input");
   const searchBtn = document.querySelector(".search-block > button");
 
-  searchBtn.addEventListener("click", (e) => {
+  searchBtn.addEventListener("click", () => {
     console.log(input.value);
   });
 };
